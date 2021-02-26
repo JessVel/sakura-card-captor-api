7f1d3a40f770754d66e304ff8ab8495130a505b8
Jesica Velazquez <jesica.velazquez@ibm.com>
2021-02-26 15:26:16 -0300

    fixing bug

--- src/server.js
+++ src/server.js
@@ -18,6 +18,12 @@ app.use(express.json({ extended: true }));
 
 const PORT = process.env.PORT || 4000;
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use("/api/card", require("./routes/card_routes"));
 
 app.listen(PORT, () => {
