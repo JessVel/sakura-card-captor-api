982c61619f1687df20db70d173a4bbac7a275eab
Jesica Velazquez <jesica.velazquez@ibm.com>
2021-02-26 15:32:23 -0300

    fixing cors

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ const app = express();
 
 connectDB();
 
+app.options("*", cors());
 app.use(cors());
 
 app.use(express.json({ extended: true }));
@@ -20,7 +21,8 @@ const PORT = process.env.PORT || 4000;
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With, Accept");
   next();
 });
 
